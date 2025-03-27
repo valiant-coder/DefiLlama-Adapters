ed64f7a387141f743b87f2b7553bdcb7331c1751
Vian <vian@exsat.org>
2025-03-27 16:37:01 +0800

    feat: finish user-points calculate

--- internal/db/db/user-points-pair.go
+++ internal/db/db/user-points-pair.go
@@ -3,14 +3,14 @@ package db
 import (
 	"context"
 	"exapp-go/data"
-
+	
 	"gorm.io/gorm"
 )
 
 func init() {
-
+	
 	addMigrateFunc(func(r *Repo) error {
-
+		
 		return r.AutoMigrate(&UserPointsPair{})
 	})
 }
@@ -22,35 +22,35 @@ type UserPointsPair struct {
 }
 
 func UserPointsPairRedisKey(pair string) string {
-
+	
 	return "user_points_pair:detail:" + pair
 }
 
 func (p *UserPointsPair) RedisKey() string {
-
+	
 	return UserPointsPairRedisKey(p.Pair)
 }
 
 type UserPointsPairListRes ListResult[UserPointsPair]
 
 func (r *Repo) ListUserPointsPair(ctx context.Context, params data.UserPointsPairListParam) (*UserPointsPairListRes, error) {
-
+	
 	res, err := List[data.UserPointsPairListParam, UserPointsPair](params)
 	if err != nil {
-
+		
 		return nil, err
 	}
-
+	
 	result := UserPointsPairListRes(res)
 	return &result, nil
 }
 
 func (r *Repo) GetUserPointsPair(ctx context.Context, pair string) (*UserPointsPair, error) {
-
+	
 	return Get[UserPointsPair](&UserPointsPair{Pair: pair})
 }
 
 func (r *Repo) CreateUserPointsPair(ctx context.Context, pair *UserPointsPair) error {
-
+	
 	return nil
 }
--- internal/services/points/user-points.go
+++ internal/services/points/user-points.go
@@ -1,15 +1,17 @@
 package points
 
 import (
+	"context"
 	"encoding/json"
 	"exapp-go/config"
 	"exapp-go/internal/db/ckhdb"
 	"exapp-go/internal/db/db"
-	"exapp-go/internal/entity"
+	"exapp-go/internal/types"
+	"exapp-go/pkg/log"
 	"exapp-go/pkg/nsqutil"
+
 	"github.com/nsqio/go-nsq"
 	"github.com/robfig/cron/v3"
-	"log"
 )
 
 const (
@@ -17,8 +19,9 @@ const (
 )
 
 type UserPointsService struct {
-	ckhRepo  *ckhdb.ClickHouseRepo
+	ctx      context.Context
 	repo     *db.Repo
+	ckhRepo  *ckhdb.ClickHouseRepo
 	consumer *nsqutil.Consumer
 }
 
@@ -33,8 +36,9 @@ func NewService() *UserPointsService {
 	repo := db.New()
 	cfg := config.Conf()
 	consumer := nsqutil.NewConsumer(cfg.Nsq.Lookupd, cfg.Nsq.LookupTTl)
-	
+
 	return &UserPointsService{
+		ctx:      context.TODO(),
 		repo:     repo,
 		ckhRepo:  ckhRepo,
 		consumer: consumer,
@@ -42,14 +46,13 @@ func NewService() *UserPointsService {
 }
 
 func (s *UserPointsService) RunConsumer() error {
-	
-	err := s.consumer.Consume(MsgTypeTradeUpdate, "points", s.HandleMessage)
-	if err != nil {
-		
-		log.Printf("Consume action sync failed: %v", err)
+
+	if err := s.consumer.Consume(MsgTypeTradeUpdate, "points", s.HandleMessage); err != nil {
+
+		log.Logger().Error("Consume action sync failed: %v", err)
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -60,23 +63,121 @@ func (s *UserPointsService) CheckTrade() {
 func (s *UserPointsService) HandleMessage(msg *nsq.Message) error {
 	var nsqMsg NSQMessage
 	if err := json.Unmarshal(msg.Body, &nsqMsg); err != nil {
-		log.Printf("Failed to unmarshal NSQ message: %v,%v", err, string(msg.Body))
+		log.Logger().Error("Failed to unmarshal NSQ message: %v,%v", err, string(msg.Body))
 		return nil // Return nil to confirm message
 	}
-	
+
 	// 非交易消息不处理
 	if nsqMsg.Type != MsgTypeTradeUpdate {
-		
+
 		return nil
 	}
-	
-	var tradeData entity.Trade
-	if err := json.Unmarshal(nsqMsg.Data, &tradeData); err != nil {
-		log.Printf("Failed to unmarshal trade data: %v", err)
+
+	var trade ckhdb.Trade
+	if err := json.Unmarshal(nsqMsg.Data, &trade); err != nil {
+		log.Logger().Error("Failed to unmarshal trade data: %v", err)
 		return nil
 	}
-	
+
+	taker, err := s.repo.GetUIDByEOSAccountAndPermission(s.ctx, trade.Taker, trade.TakerPermission)
+	if err != nil {
+
+		log.Logger().Error(trade.Taker, "@", trade.TakerPermission, "Failed to get buyer uid: %v", err)
+		return err
+	}
+
+	maker, err := s.repo.GetUIDByEOSAccountAndPermission(s.ctx, trade.Maker, trade.MakerPermission)
+	if err != nil {
+
+		log.Logger().Error(trade.Maker, "@", trade.MakerPermission, "Failed to get seller uid: %v", err)
+		return err
+	}
+
+	// 结算邀请积分
+	if err = s.SettlePoints(taker, maker, trade); err != nil {
+		log.Logger().Error(trade.PoolID, taker, "Failed to settle points: %v", err)
+		return err
+	}
+
+	// 结算返佣
+	if err = s.Rebate(taker, maker, trade); err != nil {
+		log.Logger().Error(trade.PoolID, taker, "Failed to rebate points: %v", err)
+		return err
+	}
+
 	return nil
+}
+
+func (s *UserPointsService) SettlePoints(taker, maker string, trade ckhdb.Trade) (err error) {
+	// 先计算积分
+	conf, err := s.repo.GetUserPointsConf(s.ctx)
+	if err != nil {
+
+		log.Logger().Error("get user points conf error ->", err)
+		return
+	}
+
+	// 读取交易对权重
+	pair, err := s.repo.GetUserPointsPair(s.ctx, trade.Symbol)
+	if err != nil {
+
+		log.Logger().Error("get user points pair error ->", err)
+		return
+	}
+
+	basePoints := conf.BaseTradePoints
+	quantity := uint64(trade.QuoteQuantity.IntPart())
+	takerPoints := basePoints * conf.TakerWeight * pair.Coefficient * quantity
+	makerPoints := basePoints * conf.MakerWeight * pair.Coefficient * quantity
+
+	// 更新用户积分
+	if err = s.SettlerInvitePoints(taker, takerPoints, trade); err != nil {
+
+		log.Logger().Error("settle points error ->", err)
+		return
+	}
+
+	if err = s.SettlerInvitePoints(maker, makerPoints, trade); err != nil {
+
+		log.Logger().Error("settle points error ->", err)
+		return
+	}
+
+	return
+}
+
+func (s *UserPointsService) SettlerInvitePoints(uid string, points uint64, trade ckhdb.Trade) (err error) {
+
+	err = s.repo.Transaction(s.ctx, func(repo *db.Repo) error {
+
+		if e := repo.AddTradeUserPoints(s.ctx, uid, trade.TxID, points, trade.GlobalSequence, types.UserPointsTypeTrade); e != nil {
+
+			log.Logger().Error(trade.TxID, "@", uid, "add trade user points error ->", e)
+			return e
+		}
+
+		// 查询邀请信息
+		invitation, _ := repo.GetUserInvitation(s.ctx, uid)
+		if invitation == nil {
+
+			return nil
+		}
+
+		// 查询邀请人
+		if e := repo.AddTradeUserPoints(s.ctx, invitation.Inviter, trade.TxID, points, trade.GlobalSequence, types.UserPointsTypeInvitation); e != nil {
+
+			log.Logger().Error(trade.TxID, "@", invitation.Inviter, "add trade user points error ->", e)
+			return e
+		}
+
+		return nil
+	})
+	return
+}
+
+func (s *UserPointsService) Rebate(taker, maker string, trade ckhdb.Trade) (err error) {
+
+	return
 }
 
 func Start() error {
@@ -84,18 +185,18 @@ func Start() error {
 	if err := service.RunConsumer(); err != nil {
 		return err
 	}
-	
+
 	c := cron.New(
 		cron.WithSeconds(),
 		cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)),
 	)
-	
+
 	// 扫描交易
 	if _, err := c.AddFunc("@every 3s", service.CheckTrade); err != nil {
-		
+
 		return err
 	}
-	
+
 	c.Run()
 	return nil
 }
