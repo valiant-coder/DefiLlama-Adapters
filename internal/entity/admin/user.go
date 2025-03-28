ca17876438b74345df6b9ca4c2054bc96cf792de
valiant-coder <liuw2285@gmail.com>
2025-03-28 13:10:32 +0800

    refactor(admin): Update timestamp fields to use entity.Time type for consistency

--- internal/entity/admin/user.go
+++ internal/entity/admin/user.go
@@ -2,20 +2,21 @@ package entity_admin
 
 import (
 	"exapp-go/internal/db/db"
+	"exapp-go/internal/entity"
 )
 
 type RespUser struct {
 	ID             uint           `json:"id"`
 	Username       string         `json:"username"`
 	UID            string         `json:"uid"`
-	CreatedAt      string         `json:"created_at"`
-	FirstDepositAt string         `json:"first_reposit_at"`
+	CreatedAt      entity.Time    `json:"created_at"`
+	FirstDepositAt entity.Time    `json:"first_reposit_at"`
 	LoginMethod    db.LoginMethod `json:"login_method"`
-	LastUsedAt     string         `json:"last_used_at"`
+	LastUsedAt     entity.Time    `json:"last_used_at"`
 	PasskeyCount   int            `json:"passkey_count"`
 	SecurityLevel  uint8          `json:"security_level"`
-	LastDepositAt  string         `json:"last_reposit_at"`
-	LastWithdrawAt string         `json:"last_withdraw_at"`
+	LastDepositAt  entity.Time    `json:"last_reposit_at"`
+	LastWithdrawAt entity.Time    `json:"last_withdraw_at"`
 }
 
 func (r *RespUser) Fill(a *db.UserList) *RespUser {
@@ -24,11 +25,11 @@ func (r *RespUser) Fill(a *db.UserList) *RespUser {
 	r.UID = a.UID
 	r.LoginMethod = a.LoginMethod
 	r.PasskeyCount = a.PasskeyCount
-	r.CreatedAt = a.CreatedAt.Format("2006-01-02 15:04:05")
-	r.LastUsedAt = a.LastUsedAt.Format("2006-01-02 15:04:05")
-	r.FirstDepositAt = a.FirstDepositAt.Format("2006-01-02 15:04:05")
-	r.LastDepositAt = a.LastDepositAt.Format("2006-01-02 15:04:05")
-	r.LastWithdrawAt = a.LastWithdrawAt.Format("2006-01-02 15:04:05")
+	r.CreatedAt = entity.Time(a.CreatedAt)
+	r.LastUsedAt = entity.Time(a.LastUsedAt)
+	r.FirstDepositAt = entity.Time(a.FirstDepositAt)
+	r.LastDepositAt = entity.Time(a.LastDepositAt)
+	r.LastWithdrawAt = entity.Time(a.LastWithdrawAt)
 
 	switch r.PasskeyCount {
 	case 0:
@@ -42,13 +43,13 @@ func (r *RespUser) Fill(a *db.UserList) *RespUser {
 }
 
 type RespPasskey struct {
-	Name       string `json:"name"`
-	IsAuth     bool   `json:"is_auto"`
-	Storage    string `json:"storage"`
-	Synced     bool   `json:"synced"`
-	LastUsedAt string `json:"last_used_at"`
-	LastUsedIP string `json:"last_used_ip"`
-	SignupAt   string `json:"signin_at"`
+	Name       string      `json:"name"`
+	IsAuth     bool        `json:"is_auto"`
+	Storage    string      `json:"storage"`
+	Synced     bool        `json:"synced"`
+	LastUsedAt entity.Time `json:"last_used_at"`
+	LastUsedIP string      `json:"last_used_ip"`
+	SignupAt   entity.Time `json:"signin_at"`
 }
 
 func (r *RespPasskey) Fill(a *db.UserCredential) *RespPasskey {
@@ -56,8 +57,8 @@ func (r *RespPasskey) Fill(a *db.UserCredential) *RespPasskey {
 	r.IsAuth = false
 	r.Storage = a.Storage
 	r.Synced = a.Synced
-	r.SignupAt = a.CreatedAt.Format("2006-01-02 15:04:05")
-	r.LastUsedAt = a.LastUsedAt.Format("2006-01-02 15:04:05")
+	r.SignupAt = entity.Time(a.CreatedAt)
+	r.LastUsedAt = entity.Time(a.LastUsedAt)
 	r.LastUsedIP = a.LastUsedIP
 
 	if a.EOSAccount != "" {
